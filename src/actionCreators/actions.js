d4c10ca6e3d304fb9f6fe79823a899cb3bf95d51
abdelshok <aas698@nyu.edu>
2020-11-23 17:09:31 -0500

    Remove unnecessary actions from actions.js file

--- src/actionCreators/actions.js
+++ src/actionCreators/actions.js
@@ -1,8 +1,6 @@
 import {
   AUTHENTICATE_USER,
   SIGNOUT_USER,
-  SET_SCOPE,
-  SET_LOCATION,
 } from './constants';
 
 export const authenticateUser = (state) => ({
@@ -13,14 +11,4 @@ export const authenticateUser = (state) => ({
 export const signoutUser = (state) => ({
     type: SIGNOUT_USER,
     isAuthenticated: false
-});
-
-export const setScopeAction = (scope) => ({
-  type: SET_SCOPE,
-  scope,
-});
-
-export const setLocationAction = (location) => ({
-  type: SET_LOCATION,
-  location,
 });
