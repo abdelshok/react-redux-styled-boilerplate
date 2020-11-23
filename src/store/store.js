c629cfcc819fd63af15c9ae1deec0a38e5782b05
abdelshok <aas698@nyu.edu>
2020-11-23 16:42:46 -0500

    Set up folder structure, initialize utility folder & redux store

---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Initialization of the Redux store, which will contain most of the data of our application.
+ * 
+ */
+
+// External Packages
+import { createStore } from 'redux';
+
+// Internal Modules
+import reducer from '../reducers';
+
+export default createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line
+);
+  
---
+++ src/utility/index.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Utility folder, as the name suggests, will store the different utility functions that will be used throughout the application.
+ * 
+ * 
+ */
+
+//  export { 
+
+//  } from './';
