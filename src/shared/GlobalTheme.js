1c40c50819f8fde4f112fea37d8a7d2d710aec65
abdelshok <aas698@nyu.edu>
2020-11-23 17:49:58 -0500

    feat: add global theming through styled components for the application

---
+++ src/shared/GlobalTheme.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Central Source of truth for the theme-ing / interface of the application
+ * 
+ */
+
+const GlobalTheme = {
+    generalApplication: {
+      modalBorderRadius: '',
+      paragraphTextFontFamily: '',
+      headerTextFontFamily: '',
+      paragraphTextFontSize: '',
+      headerTextFontSize: '',
+      paragraphTextColor: '',
+      headerTextColor: '',
+      lightBackgroundColor: '',
+      darkBackgroundColor: '',
+    },
+    generalColors: {
+      
+    },
+  };
+  
+  
+  export default GlobalTheme;
+  
